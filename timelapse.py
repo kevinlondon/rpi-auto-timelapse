4666f3d0943a4473d3df3e2b16108d23a37fb151
Kevin London <kevinlondon@gmail.com>
2014-08-22 15:34:00 +0000

    Added handle for controlling a raspberry pi builtin camera.

--- timelapse.py
+++ timelapse.py
@@ -4,6 +4,7 @@ import os
 import datetime
 import time
 from subprocess import Popen, PIPE
+import picamera
 
 # For easier datetimes.
 import arrow
@@ -16,17 +17,25 @@ def start(city_name="Los Angeles", folder="images"):
     delay = get_timelapse_delay(dawn, dusk)
     tzinfo = dawn.tzinfo
 
+    # Get the datetime so we can make a folder for it.
+    raw_time = arrow.now(tzinfo)
+    formatted_time = raw_time.format("YYYY_MM_DD-HH_mm_ss")
+
+    # Build the folder path for datetime.
+    image_folder = os.path.join(folder, formatted_time)
     if not os.path.exists(folder):
         os.makedirs(folder)
 
+    # Initialize camera so we don't have to get a new handle to it
+    # each time we want to use it.
+    camera = picamera.PiCamera()
+
     shot = 0
     while arrow.now(tzinfo) < dusk:
-        raw_time = arrow.now(tzinfo)
-        formatted_time = raw_time.format("YYYY-MM-DD_HH:mm:ss")
         print("Taking picture {0} at {1}".format(shot, formatted_time))
-        filename = "timelapse_{0}.jpg".format(formatted_time)
+        filename = "timelapse_{0:04d}.jpeg".format(shot)
         imgpath = os.path.join(folder, filename)
-        take_picture(imgpath)
+        take_picture(imgpath, camera)
         shot += 1
         time.sleep(delay)
 
@@ -64,9 +73,13 @@ def get_timelapse_delay(dawn, dusk, total_runtime="00:00:06", fps=24):
     return frame_delay
 
 
-def take_picture(filename):
+def take_picture(filename, camera):
+    """Use the raspberry pi builtin camera."""
+    camera.capture(filename)
+
+
+def take_gphoto_picture(filename):
     """Use gphoto2 to interact with our camera and take the picture."""
-    print filename
     process = Popen([
         "/usr/local/bin/gphoto2", "--capture-image-and-download",
         "--filename", filename
